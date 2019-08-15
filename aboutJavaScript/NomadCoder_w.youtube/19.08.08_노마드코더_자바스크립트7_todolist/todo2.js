f40d8d90dd2cc3d59e9c4a332a41d2b67c99363d
hayuni5 <ghldnjscool@gmail.com>
2019-08-15 20:21:03 +0900

    190815 20:20 MakingToDo(2) 마무리

--- aboutJavaScript/NomadCoder_w.youtube/19.08.08_노마드코더_자바스크립트7_todolist/todo2.js
+++ aboutJavaScript/NomadCoder_w.youtube/19.08.08_노마드코더_자바스크립트7_todolist/todo2.js
@@ -20,6 +20,22 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, /* toDos */ JSON.stringify(toDos));
+    /* 
+    A JavaScript value, usually an object or array, to be converted.
+    Converts a JavaScript value to a JavaScript Object Notation (JSON)
+    string.
+    */
+    /* 로컬 스토리지에 setItem(키명, 값에 해당하는 어떤 것-변수 등) 했을 때에
+        키 이름은 제대로 들어가는데,
+        정확한 데이터는 값으로 저장할 수 없다.
+        로컬 스토리지는 문자열만 값으로 저장할 수 있다.
+        그래서 JSON.stringify() 를 쓴다. 자바스크립트 문서 내 객체나 배열 값을
+        문자열로 변환해준다.
+    */
+}
+
 function paintToDo(text){ //handleSubmit이 호출한 함수
     const li = document.createElement("li"); 
     const delBtn = document.createElement("button"); //
@@ -38,8 +54,8 @@ function paintToDo(text){ //handleSubmit이 호출한 함수
         id: newId
     };
     toDos.push(toDoObj);    // Put element inside the array by this code.
+    saveToDos();            //Be sure you call this function after you push the toDoObj element
 }
-
 function handleSubmit(event) {
     event.preventDefault(); //submit이벤트 발생시 데이터를 다른 url로 전송시키지 말고..
     const currentValue = toDoInput.value;// input태그에 입력된 값을 변수로 만들어 담고,
@@ -47,10 +63,16 @@ function handleSubmit(event) {
     toDoInput.value = ""; //input태그의 값을 없앰
 }
 
-function loadToDos() {
-    const loadedtoDos = localStorage.getItem(TODOS_LS);
-    if(loadedtoDos!==null){
 
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos!==null){
+        console.log(loadedToDos);
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+            /* .forEach() -> 배열의 각 원소마다 주어진 함수를 실행 */
+        });
     } 
 }
 
